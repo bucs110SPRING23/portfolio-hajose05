d3c90e628238bc7d2cdddcd234adefa6a0326241
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-09-08 13:57:05 -0400

    Update main.py

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,15 @@
-
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week) 
+classes_per_week = 5 
+cost_per_class = cost_per_week / classes_per_week 
+print('You owe ',cost_per_class,' carbon based organism')  
+print(weeks, type(weeks)) 
+print(classes, type(weeks))  
+print(tuition, type(tuition)) 
+print(cost_per_week, type(cost_per_week)) 
+print(classes_per_week, type(classes_per_week)) 
+print(cost_per_class, type(cost_per_class)) 
