e86811fecc639154d12b7f5a5e3cf08ec6fd7b07
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-09-18 18:31:32 -0400

    Update drawing.py

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,1 +1,10 @@
-hello
+import turtle 
+side_number = input('We are making a shape. How many sides should it have ') 
+side_length = input('How long should each side be? ')  
+internal_angle = 360/(int(side_number)) 
+Leonardo = turtle.Turtle() 
+Leonardo.shape('triangle') 
+Leonardo.color('purple') 
+for _ in range(int(side_number)): 
+  Leonardo.forward(int(side_length)) 
+  Leonardo.left(internal_angle)  
