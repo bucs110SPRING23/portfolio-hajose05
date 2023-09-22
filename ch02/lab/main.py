ca2dde53c31511ce6fe0276ddc7b28ef53680988
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-09-22 16:05:11 -0400

    Update main.py

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,38 @@
+import turtle  
+import random
 
-#Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+#RACE 1 
+window = turtle.Screen() 
+window.bgcolor("lightblue")
 
+michelangelo = turtle.Turtle() 
+leonardo = turtle.Turtle()
+michelangelo.color("orange")
+leonardo.color("blue")
+michelangelo.shape("turtle")
+leonardo.shape("turtle")
 
-#Part B
+michelangelo.up()  
+leonardo.up()
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
+michelangelo.forward(99) 
+leonardo.forward(90) 
+michelangelo.right(180)
+leonardo.right(180) 
+michelangelo.goto(-100,20) 
+leonardo.goto(-100,-20)  
+michelangelo.right(180) 
+leonardo.right(180) 
+
+#RACE 2 
+for _ in range(20): 
+  leonardo.forward(random.randrange(1,15)) 
+  michelangelo.forward(random.randrange(1,20)) 
+michelangelo.right(180) 
+leonardo.right(180) 
+michelangelo.goto(-100,20) 
+leonardo.goto(-100,-20)  
+michelangelo.right(180) 
+leonardo.right(180) 
+window.exitonclick()
