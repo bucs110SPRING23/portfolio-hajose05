e42a8984cabf7908afcf4dea398013b86093510b
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-10-02 11:36:16 -0400

    Update data.py

---
+++ ch01/exercises/data.py
@@ -0,0 +1,11 @@
+print(10*5) 
+print(10**2) 
+print(15/10) 
+print(15//10) 
+print(-15//10) 
+print(15%10) 
+print(10%15) 
+print(10%10) 
+print(0%10) 
+print(10/15) 
+#my computer can only recogonise number us to 2^64. so although 0.6 is recurring to infinity, the computer gives an un rounded estimate 
