e71bb394ff629c2d2f02b34432a267adda2ba9a0
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-10-17 14:41:14 -0400

    Update main.py

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -36,3 +36,34 @@ leonardo.goto(-100,-20)
 michelangelo.right(180) 
 leonardo.right(180) 
 window.exitonclick()
+
+# PART B 
+import pygame 
+import math 
+pygame.init() 
+screen = pygame.display.set_mode((600,600)) 
+screen.fill('black') 
+pygame.display.flip()  
+
+num_of_sides = [3,4,6,20,100,360,100000] 
+side_length = 100
+xpos = 300 
+ypos = 300
+points = []
+for j in range(0,7): 
+        side = num_of_sides[j]
+        for i in range(0,side): 
+            angle = 360/side 
+            radians = math.radians(angle * i) 
+            x = xpos + side_length * math.cos(radians) 
+            y = ypos + side_length * math.sin(radians) 
+            points.append((x,y)) 
+
+        pygame.draw.polygon(screen,'red',points) 
+        pygame.display.flip() 
+        pygame.time.wait(1000) 
+        screen.fill('black') 
+        pygame.display.flip()  
+        points = [] 
+     
+        
