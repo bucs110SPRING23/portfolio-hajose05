ef4907053ac6e929207e20ef97840edd6ee04a4b
hajose05 <143661406+hajose05@users.noreply.github.com>
2023-10-23 14:08:38 -0400

    Update main.py

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,23 +1,26 @@
-import math 
+#LAB PART A 
 import pygame 
-import random 
-pygame.display.init() 
-screen = pygame.display.set_mode()
-screen_size = pygame.display.get_window_size() 
-w = screen_size[1] 
-l = screen_size[0]
-background = pygame.color.Color('black') 
-screen.fill(background)
-print(screen_size)  
-pygame.draw.circle(screen,'red',(screen_size[0]/2,screen_size[1]/2),screen_size[0]/2) 
-pygame.draw.line(screen,'black',(0,w/2),(l,w/2)) 
-pygame.draw.line(screen,'black',(l/2,0),(l/2,w))
-pygame.display.flip()
-pygame.time.delay(1000)  
-
-for x in range(0,10): 
-    cord_l = random.randrange(0,l) 
-    cord_w = random.randrange(0,w) 
-    pygame.draw.circle(screen,'blue',(cord_l,cord_w),10)
-    pygame.display.flip()
-    pygame.time.delay(300)  
+import math 
+import random
+screen = pygame.display.set_mode((800, 800)) 
+screen.fill('blue') 
+pygame.display.flip() 
+pygame.draw.circle(screen, 'red', (400, 400), 400)
+pygame.display.flip() 
+pygame.draw.line(screen, 'black', (0, 400), (800, 400), 5 )
+pygame.draw.line(screen, 'black', (400, 0), (400, 800), 5 ) 
+pygame.display.flip() 
+#LAB PART B
+x1 = 400 
+y1 = 400 
+for i in range(1,10): 
+    x2 = random.randrange(0, 800) 
+    y2 = random.randrange(0, 800) 
+    distance_from_center = math.hypot(x1-x2, y1-y2) 
+    dart_is_in_circle = distance_from_center <= 800/2 
+    if dart_is_in_circle: 
+        pygame.draw.circle(screen,'green', (x2, y2), 20) 
+        pygame.display.flip() 
+    else: 
+        pygame.draw.circle(screen, 'magenta', (x2, y2), 20) 
+        pygame.display.flip()
